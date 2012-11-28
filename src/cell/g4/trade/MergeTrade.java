9a114e478a11fdc21aa0a78409945acf426e565c
Albert Lee <akl2140@columbia.edu>
2012-11-27 20:14:21 -0500

    changed riskyTrading as a default in mergeTrading

--- src/cell/g4/trade/MergeTrade.java
+++ src/cell/g4/trade/MergeTrade.java
@@ -6,15 +6,17 @@ import cell.g4.Sack;
 public class MergeTrade extends TradeAlgo {
 	private MaxRateDiffTrade diffTrading;
 	private WinningTrade winningTrading;
+	private RiskyTrade riskTrading;
 	private TradeAlgo current;
 	
 	
 	public MergeTrade(Board board, Sack sack) {
 		super(board, sack);
 		diffTrading = new MaxRateDiffTrade(board, sack);
+		riskTrading = new RiskyTrade(board, sack);
 		winningTrading = new WinningTrade(board, sack);
 		
-		current = diffTrading;
+		current = riskTrading;
 	}
 
 	@Override
