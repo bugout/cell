928fb7e860582ee41f4012aeb2452eb74cbc1e5f
Jiacheng Yang <jiachengy.yang@gmail.com>
2012-12-01 13:28:47 -0500

    merge BoardAnalyzer into Board

--- src/cell/g4/Board.java
+++ src/cell/g4/Board.java
@@ -15,6 +15,50 @@ public class Board {
 		this.board = board;
 	}
 	
+	public int dimension(){
+		return board.length;
+	}
+	
+	public double[] getColorDistribution(){
+		double[] color= new double[6];
+		for(int i=0; i < board.length ; i++){
+			for(int j=0; j < board[0].length; j++){
+				switch (board[i][j]) {
+				case 0:
+					color[0]++;
+					break;
+				case 1:
+					color[1]++;
+					break;
+				case 2:
+					color[2]++;
+					break;
+				case 3:
+					color[3]++;
+					break;
+				case 4:
+					color[4]++;
+					break;
+				case 5:
+					color[5]++;
+					break;
+				default:
+					break;
+				}
+			}
+		}
+		
+		int sum=0;
+		for(int i=0;i<color.length;i++){
+			sum+=color[i];
+		}
+		
+		for(int i=0; i <color.length;i++){
+			color[i] /= sum;
+		}
+		return color;
+	}
+	
 	public int getColor(int[] location) {
 		int i = location[0];
 		int j = location[1];
--- src/cell/g4/DynamicSack.java
+++ src/cell/g4/DynamicSack.java
@@ -1,24 +1,15 @@
 package cell.g4;
 
-import java.util.Arrays;
-
 public class DynamicSack extends Sack {
-	BoardAnalyzer ba = new BoardAnalyzer();
 	double[] dist;
 	int dimension;
 	public DynamicSack(int[] sack,Board board){
 		super(sack,board);
 
-		dist = ba.getColorDistribution(board.getBoard());
-		dimension = ba.dimensionOfMap(board.getBoard());
+		dist = board.getColorDistribution();
+		dimension = board.dimension();
 		
 		updateReserve();
-	}
-
-	@Override
-	public int getReserve(int color) {
-		//udpate the resrve here
-		return super.getReserve(color);
 	}
 
 	private void updateReserve(){
@@ -27,9 +18,7 @@ public class DynamicSack extends Sack {
 			reserve[i]= (int) (dist[i] * getStock(i) * 3);
 		}
 		super.reserves = reserve;
-
 	}
-
 }
 
 
